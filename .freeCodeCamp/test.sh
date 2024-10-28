ae623ebdd869778b91e58f5bc31320a6bd4dd8a5
azmi anindya <azmiindya29@gmail.com>
2024-10-28 07:56:20 +0000

    2404006

---
+++ .freeCodeCamp/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Bingo Number Generator
+echo -e "\n~~ Bingo Number Generator ~~\n"
+NUMBER=$(( RANDOM % 15+16 ))
+TEXT="The next number is, "
+elif [[ $NUMBER -le 30 ]]
+then
+  echo $TEXT I:$NUMBER
+f
